0abf10c7bd1d141959c46c9b293f4ba79a10b8fb
Ilya Kolpakov <ilya.kolpakov@gmail.com>
2018-05-24 20:17:50 +0200

    fix local development mode

--- shell.nix
+++ shell.nix
@@ -5,38 +5,23 @@ let
   from = where: what: getAttr what where;
 
   v = python-version;
-  python = from pkgs "python${v}";
-  pythonPackages = from pkgs "python${v}Packages";
-  
-  package-names = [
-    "ipython"
-    #"tkinter" # support %paste in ipython
-    "numpy"
-    "scipy"
-    "pandas"
-    "sympy"
-    "networkx"
-    #"matplotlib"
-    #"seaborn" # for pymc plots primarily
-    #"imageio"
-    #"ipykernel"
-    #"ipywidgets"
-    "notebook"
-    #"pymc3"
-    #"scikitlearn"
+  py = from pkgs "python${v}";
+  pyp = from pkgs "python${v}Packages";
+
+  extra = with pyp; [
+    ipython
+    numpy
+    scipy
+    pandas
+    sympy
+    notebook
   ];
 
-  bayex = pythonPackages.buildPythonPackage rec {
-    name = "bayex";
-    src = ./.;
-    propagatedBuildInputs = with pythonPackages; [ networkx pytest ];
-  };
+in pyp.buildPythonPackage rec {
 
-  packages = map (from pythonPackages) package-names ++ [bayex];
-
-in pkgs.stdenv.mkDerivation {
-
-  name = "py${v}env";
-  buildInputs = [python] ++ packages;
+  name = "bayex";
+  src = ./.;
+  propagatedBuildInputs = with pyp; [ networkx pytest ] ++ extra;
+  doCheck = false;
 
 }
